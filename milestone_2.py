53a2d603cb524998f8b0df62c3b92bea039944eb
Mental <im3ntaly@gmail.com>
2023-10-18 13:43:18 +0100

    creates script and adds basic functionality.

---
+++ milestone_2.py
@@ -0,0 +1,10 @@
+import random
+word_list = ["Apple", "Kiwi", "Pear", "Strawberry", "Tomato"]
+word = random.choice(word_list)
+
+
+guess = input("Guess: ")
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
